8eaced2b3e877b3f4b8807fe9a973cc41c20bdfa
onyiimesi <onyedikachukwu62@gmail.com>
2024-05-27 08:39:50 +0100

    table responsive

--- resources/views/dashboard/mealplanview.blade.php
+++ resources/views/dashboard/mealplanview.blade.php
@@ -15,50 +15,53 @@
                         </div>
                         @forelse ($groupedPlans as $type => $plans)
                             <h5>{{ $type }}</h5>
-                            <table id="datatable" class="table table-bordered dt-responsive nowrap table-responsive"
-                                style="border-collapse: collapse; border-spacing: 0; width: 100%;">
-                                <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Name</th>
-                                        <th>Ingredients</th>
-                                        <th>Calories</th>
-                                        <th>Price</th>
-                                        <th>Carbohydrate</th>
-                                        <th>Protein</th>
-                                        <th>Fat</th>
-                                        <th>Procedure</th>
-                                        <th>Image Rectangular</th>
-                                        <th>Image Oval</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @forelse ($plans as $mealplan)
+                            <div class="table-responsive">
+                                <table id="datatable" class="table table-bordered dt-responsive nowrap"
+                                    style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                                    <thead>
                                         <tr>
-                                            <td>{{ $loop->iteration }}</td>
-                                            <td>{{ $mealplan->name }}</td>
-                                            <td>{{ $mealplan->ingredients }}</td>
-                                            <td>{{ $mealplan->calories }}</td>
-                                            <td>{{ $mealplan->price }}</td>
-                                            <td>{{ $mealplan->carbohydrate }}</td>
-                                            <td>{{ $mealplan->protein }}</td>
-                                            <td>{{ $mealplan->fat }}</td>
-                                            <td>{{ $mealplan->procedure }}</td>
-                                            <td><img src="{{ $mealplan->image_rectangular }}" width="50" height="50" alt=""></td>
-                                            <td><img src="{{ $mealplan->image_oval }}" width="50" height="50" alt=""></td>
+                                            <th>#</th>
+                                            <th>Name</th>
+                                            <th>Ingredients</th>
+                                            <th>Calories</th>
+                                            <th>Price</th>
+                                            <th>Carbohydrate</th>
+                                            <th>Protein</th>
+                                            <th>Fat</th>
+                                            <th>Procedure</th>
+                                            <th>Image Rectangular</th>
+                                            <th>Image Oval</th>
                                         </tr>
-                                    @empty
-                                        <tr class="text-center">
-                                            <td colspan="9">No data available.</td>
-                                        </tr>
-                                    @endforelse
-                                </tbody>
-                            </table>
+                                    </thead>
+                                    <tbody>
+                                        @forelse ($plans as $mealplan)
+                                            <tr>
+                                                <td>{{ $loop->iteration }}</td>
+                                                <td>{{ $mealplan->name }}</td>
+                                                <td>{{ $mealplan->ingredients }}</td>
+                                                <td>{{ $mealplan->calories }}</td>
+                                                <td>{{ $mealplan->price }}</td>
+                                                <td>{{ $mealplan->carbohydrate }}</td>
+                                                <td>{{ $mealplan->protein }}</td>
+                                                <td>{{ $mealplan->fat }}</td>
+                                                <td>{{ $mealplan->procedure }}</td>
+                                                <td><img src="{{ $mealplan->image_rectangular }}" width="50" height="50" alt=""></td>
+                                                <td><img src="{{ $mealplan->image_oval }}" width="50" height="50" alt=""></td>
+                                            </tr>
+                                        @empty
+                                            <tr class="text-center">
+                                                <td colspan="9">No data available.</td>
+                                            </tr>
+                                        @endforelse
+                                    </tbody>
+                                </table>
+                            </div>
                         @empty
                             <h5>No data available.</h5>
                         @endforelse
 
                         <h5>Snacks</h5>
+                        <div class="table-responsive">
                             <table id="datatable-snacks" class="table table-bordered dt-responsive nowrap"
                                 style="border-collapse: collapse; border-spacing: 0; width: 100%;">
                                 <thead>
@@ -88,7 +91,7 @@
                                     @endforelse
                                 </tbody>
                             </table>
-
+                        </div>
                     </div>
                 </div>
             </div>
